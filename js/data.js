2776898ad21f90f8b49876fbcae292bcc7c713f2
mayself <azimovsarvar188@gmail.com>
2022-11-02 19:34:46 +0500

    Song-1

---
+++ js/data.js
@@ -0,0 +1,32 @@
+let song = [
+  {
+    name: 'song 1',
+    path: 'music/Song 1.mp3',
+    artist: 'artist 1',
+    cover: 'img/cover 1.png'
+  },
+{
+  name: 'song 2',
+  path: 'music/Song 2.mp3',
+  artist: 'artist 2',
+  cover: 'img/cover 2.png'  
+},
+{
+    name: 'song 3',
+    path: 'music/Song 3.mp3',
+    artist: 'artist 3',
+    cover: 'img/cover 3.png'  
+  },
+  {
+    name: 'song 4',
+    path: 'music/Song 4.mp3',
+    artist: 'artist 4',
+    cover: 'img/cover 4.png'  
+  },
+  {
+    name: 'song 5',
+    path: 'music/Song 5.mp3',
+    artist: 'artist 5',
+    cover: 'img/cover 5.png'  
+  }          
+]
